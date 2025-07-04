5f9940a57d7dad21951b3259cbdfd0a43b467227
8290997456 <mrvivekkumar05@gmail.com>
2025-07-04 22:45:01 +0530

    apichange

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-// src > app.js 
+// src > app.js
 import express from 'express';
 import cors from 'cors';
 import imageRoutes from './routes/imageRoutes.js';
@@ -11,12 +11,15 @@ import ocrRoutes from './routes/ocrRoutes.js';
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.get('/api/ping', (req, res) => {
+  res.status(200).send('Pong');
+});
 
 app.use('/remove-password', removePasswordRoutes);
 app.use('/file-convert', docConvertRoutes);
 app.use('/convert', convertRoutes);
 app.use('/compress', imageRoutes);
-app.use('/crop-image',cropImageRoute);
+app.use('/crop-image', cropImageRoute);
 app.use('/ocr', ocrRoutes);
 
 export default app;
